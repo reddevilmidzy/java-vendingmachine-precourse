badb7b6f71208a1bc81990c9ff6585fbf6113c7d
은지 <midzy0228@gmail.com>
2022-12-16 00:50:41 +0900

    feat(InputView): 상품명과 가격 수량 입력 기능 구현

--- src/main/java/vendingmachine/View/InputView.java
+++ src/main/java/vendingmachine/View/InputView.java
@@ -3,11 +3,18 @@ package vendingmachine.View;
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.model.Product;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    private static final int COUNT_INDEX = 2;
+    private static final int NAME_INDEX = 0;
+    private static final int PRICE_INDEX = 1;
+    private static final String CLOSING_PARENTHESIS = "]";
     private static final String INDIVIDUAL_PRODUCT_SEPARATE = ";";
+    private static final String OPEN_PARENTHESIS = "[";
+    private static final String PRODUCT_OPTION_SEPARATE = ",";
 
     public Integer inputVendingMachineAmount() {
         String input = readInput();
@@ -17,6 +24,16 @@ public class InputView {
     public Integer inputAmount() {
         String input = readInput();
         return validateInteger(input);
+    }
+
+    public List<Product> inputMerchandise() {
+        List<Product> result = new ArrayList<>();
+        String input = readInput();
+        List<String> merchandises = Arrays.asList(input.split(INDIVIDUAL_PRODUCT_SEPARATE));
+        for (String merchandise : merchandises) {
+            result.add(splitOption(merchandise));
+        }
+        return result;
     }
 
     private String readInput() {
@@ -29,5 +46,13 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수를 입력해주세요.");
         }
+    }
+
+    private Product splitOption(String merchandise) {
+        List<String> result = List.of(merchandise.replace(OPEN_PARENTHESIS, "")
+                .replace(CLOSING_PARENTHESIS, "").split(PRODUCT_OPTION_SEPARATE));
+        Integer price = validateInteger(result.get(PRICE_INDEX));
+        Integer count = validateInteger(result.get(COUNT_INDEX));
+        return new Product(result.get(NAME_INDEX), price, count);
     }
 }
