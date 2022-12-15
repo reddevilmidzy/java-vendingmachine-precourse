2e7712dff6cee4789757d3c03121f7f99c6d6e34
은지 <midzy0228@gmail.com>
2022-12-16 01:57:17 +0900

    feat(CoinMaker): 자판기 보유 금액으로 동전 무작위 생성

---
+++ src/main/java/vendingmachine/model/Coin.java
@@ -0,0 +1,26 @@
+package vendingmachine.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private final int amount;
+
+    Coin(final int amount) {
+        this.amount = amount;
+    }
+
+    // 추가 기능 구현
+    public static List<Integer> getCoins() {
+        List<Integer> result = new ArrayList<>();
+        for (Coin coin : Coin.values()) {
+            result.add(coin.amount);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/vendingmachine/model/CoinMaker.java
@@ -0,0 +1,21 @@
+package vendingmachine.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CoinMaker {
+    public List<Integer> makeCoin(Integer vendingMachineAmount) {
+        List<Integer> result = new ArrayList<>();
+        while (vendingMachineAmount != 0) {
+            int coin = Randoms.pickNumberInList(Coin.getCoins());
+            if (vendingMachineAmount - coin >= 0) {
+                vendingMachineAmount -= coin;
+                result.add(coin);
+            }
+        }
+        return result;
+    }
+}
