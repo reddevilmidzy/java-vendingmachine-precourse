9a7fd0c54df8d86e89f6dccaf53ecab62f02ed98
은지 <midzy0228@gmail.com>
2022-12-16 02:25:35 +0900

    feat(Conltroller): 컨트롤러와 연결

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,24 +1,29 @@
 package vendingmachine.controller;
 
 import vendingmachine.View.InputView;
+import vendingmachine.View.OutputView;
+import vendingmachine.model.CoinMaker;
 import vendingmachine.model.Product;
 
 import java.util.List;
 
 public class VendingMachineController {
     private final InputView inputView;
+    private final OutputView outputView;
 
     public VendingMachineController() {
         this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     public void run() {
         Integer vendingMachineAmount = inputView.inputVendingMachineAmount();
-        //ToDO: 자판기 동전으로 변경
+        List<Integer> machineHoldingMoney = CoinMaker.makeCoin(vendingMachineAmount);
+        outputView.printMachineHoldingMoney(machineHoldingMoney);
         List<Product> merchandises = inputView.inputMerchandise();
         Integer amount = inputView.inputAmount();
         //ToDo: 구매 가능할 떄까지 반복
         String ToBuyProduct = inputView.inputToBuyProduct();
-
+        outputView.printChanges(machineHoldingMoney);
     }
 }
